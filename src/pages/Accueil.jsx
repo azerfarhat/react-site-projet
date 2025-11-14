15dc3315e7f2816ce1bbfb1b64a5e98711f936f8
yassmine-hmaidi <yassminehmaidi11@gmail.com>
2025-11-15 00:08:28 +0100

    Update Accueil.jsx

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -2,12 +2,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
-import WhyUs from '../components/WhyUs';
+import WhyUs from '../components/WhyUs';  
 import Contact from '../components/Contact';
 
 function Accueil() {
     return (
-    <>
+    <>  {/* UN SEUL FRAGMENT */}
         <Hero />
         <Services />
         <WhyUs />
